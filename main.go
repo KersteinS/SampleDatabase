c5e7adb109736d8d7ce9b34b4f630631ebe0edd5
Seth Kerstein (Desktop) <179616615+KersteinS@users.noreply.github.com>
2024-08-27 19:03:39 -0400

    began implementing custom schema. Stuck on using UserID as a foreign key in other tables.

--- main.go
+++ main.go
@@ -3,113 +3,175 @@ package main
 import (
 	"bufio"
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const dbName = "./sample.db?_foreign_keys=on"
+
+func createDatabase(db *sql.DB) { //add not null to most things in sqlStmt
+	sqlStmt := `
+	create table Weekdays (
+		DayID integer primary key autoincrement,
+		DayName text
+	);
+	create table Months (
+		MonthID integer primary key autoincrement,
+		MonthName text not null
+	);
+	create table Dates (
+		DateID integer primary key autoincrement,
+		Month integer not null,
+		Day integer not null,
+		Year integer not null,
+		foreign key (Month) references Months(MonthID),
+		foreign key (Day) references Weekdays(DayID)
+	);
+	create table Users (
+		UserID text primary key,
+		Password blob(64) not null
+	) without rowid;
+	create table Volunteers (
+		VolunteerID integer primary key autoincrement,
+		foreign key (UserID) references Users(UserID),
+		VolunteerName text not null
+	);
+	`
+	/*create table Schedules (
+		ScheduleID integer primary key autoincrement,
+		foreign key (UserID) references Users(UserID),
+		ScheduleName text not null,
+		foreign key (StartDate) references Dates(DateID),
+		foreign key (EndDate) references Dates(DateID),
+		ShiftsOff integer not null,
+		VolunteersPerShift integer not null
+	);
+	create table WeekdaysForSchedule (
+		WFSID integer primary key autoincrement,
+		foreign key (UserID) references Users(UserID),
+		foreign key (Weekday) references Weekdays(DayID),
+		foreign key (Schedule) references Schedules(ScheduleID)
+	);
+	create table VolunteersForSchedule (
+		VFSID integer primary key autoincrement,
+		foreign key (UserID) references Users(UserID),
+		foreign key (Schedule) references Schedules(ScheduleID),
+		foreign key (Volunteer) refernces Volunteers(VolunteerID)
+	);
+	create table UnavailabilitiesForSchedule (
+		UFSID integer primary key autoincrement,
+		foreign key (UserID) references Users(UserID),
+		foreign key (VolunteerForSchedule) references VolunteersForSchedule(VFSID),
+		foreign key (Date) refernces Dates(DateID)
+	);
+	create table CompletedSchedules (
+		CScheduleID integer primary key autoincrement,
+		foreign key (UserID) references Users(UserID),
+		foreign key (Schedule) references Schedules(ScheduleID),
+		ScheduleData text not null
+	);
+	`*/
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tx.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		log.Fatal(err)
+	}
+	/*stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	for i := 0; i < 100; i++ {
+		_, err = stmt.Exec(i, fmt.Sprintf("こんにちは世界%03d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}*/
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func initDatabase() (*sql.DB, error) { // https://github.com/mattn/go-sqlite3/blob/master/_example/simple/simple.go
 	dbExists := false
-	if _, err := os.Stat("./vsa.db"); err == nil {
+	if _, err := os.Stat(dbName); err == nil {
 		dbExists = true
 	}
-	db, err := sql.Open("sqlite3", "./vsa.db")
+	db, err := sql.Open("sqlite3", dbName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 	if !dbExists {
-		sqlStmt := `
-	create table foo (id integer not null primary key, name text);
-	delete from foo;
-	`
-		_, err = db.Exec(sqlStmt)
-		if err != nil {
-			log.Printf("%q: %s\n", err, sqlStmt)
-			return db, err
-		}
-		tx, err := db.Begin()
+		createDatabase(db)
+	}
+	/*
+		rows, err := db.Query("select id, name from foo")
 		if err != nil {
 			log.Fatal(err)
 		}
-		stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+		defer rows.Close()
+		for rows.Next() {
+			var id int
+			var name string
+			err = rows.Scan(&id, &name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(id, name)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		stmt, err := db.Prepare("select name from foo where id = ?")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer stmt.Close()
-		for i := 0; i < 100; i++ {
-			_, err = stmt.Exec(i, fmt.Sprintf("こんにちは世界%03d", i))
+		var name string
+		err = stmt.QueryRow("3").Scan(&name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(name)
+
+		_, err = db.Exec("delete from foo")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rows, err = db.Query("select id, name from foo")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var id int
+			var name string
+			err = rows.Scan(&id, &name)
 			if err != nil {
 				log.Fatal(err)
 			}
+			fmt.Println(id, name)
 		}
-		err = tx.Commit()
+		err = rows.Err()
 		if err != nil {
 			log.Fatal(err)
-		}
-	}
-
-	rows, err := db.Query("select id, name from foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	stmt, err := db.Prepare("select name from foo where id = ?")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	var name string
-	err = stmt.QueryRow("3").Scan(&name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(name)
-
-	_, err = db.Exec("delete from foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err = db.Query("select id, name from foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+		}*/
 	return db, nil
 }
 
